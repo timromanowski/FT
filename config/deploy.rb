abe785d4bbf12ba6bee6071c90b32e648d1bfaf7
Tim Romanowski <tim.romanowski@active911.com>
2016-04-08 20:34:27 -0700

    saving

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,5 +47,15 @@ namespace :deploy do
       # end
     end
   end
+  
+  desc 'Restart application'
+    task :restart do
+      on roles(:app), in: :sequence, wait: 5 do
+        execute :touch, release_path.join('tmp/restart.txt')
+      end
+    end
+
+    after :publishing, 'deploy:restart'
+    after :finishing, 'deploy:cleanup'
 
 end
