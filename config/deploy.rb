6d43a3a3b0347aae1dcbd3c7d1d64150d18bfdb5
Tim Romanowski <tim.romanowski@active911.com>
2016-04-11 18:46:35 -0700

    saving

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,13 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 namespace :deploy do
 
+    task :seed do
+    desc "reload the database with seed data"
+      task :seed do
+        run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+      end
+    end
+  
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
