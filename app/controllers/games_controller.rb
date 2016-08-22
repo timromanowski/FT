79ae53206c10da5911cc5ee39b42837d2bdc1c04
Timothy Romanowski <Tim@Timothys-MacBook-Pro-2.local>
2016-08-21 18:43:38 -0700

    a

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,8 +24,7 @@ class GamesController < ApplicationController
     # http://ruby-doc.org/stdlib-2.1.1/libdoc/time/rdoc/Time.html#method-c-parse
     @date = params[:date].nil? ?  nil : Time.parse( params[:date] )
     @games = @date.nil? ? Game.on_utc_date( @utc_date ) : Game.on_date( @date )   
-    byebug
-		respond_to do |format|
+    respond_to do |format|
 			format.html { render :partial => 'partials/gamelist' }
 			format.json { render json: @games }  # respond with the created JSON object
 		end
